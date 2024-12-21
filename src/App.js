4bacad209a3ed4feb527492017eb0d3149b5685e
Vaishnavi-Kolipaka <kolipakavaishnavi7@gmail.com>
2024-12-21 22:55:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import "./output.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginComponent from "./routes/Login";
+
+function App() {
+  return (
+    <div className="w-screen h-screen font-poppins">
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloComponent/>}/>
+          <Route path="/login" element={<LoginComponent/>}/>
+        </Routes>
+     
+     </BrowserRouter>
+    </div>
+  );
+}
+ const HelloComponent = () => {
+  return <div>This hello from component</div>
+ };
+
+export default App;
